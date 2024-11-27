3e3596a304a73ff5bc350d0649867665788ce7a5
尾花 <47906698+leonardoNael@users.noreply.github.com>
2024-11-27 02:28:24 -0300

    Card de colaborador estático

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/ObanaThird.png' alt='Leonardo Obana' />
+            </div>
+            <div className='rodape'>
+                <h5>Leonardo Obana</h5>
+                <h4>Desenvolvedor</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
