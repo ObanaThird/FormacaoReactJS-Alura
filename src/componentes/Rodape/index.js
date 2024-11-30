bf5bce3f4bc8cc3fbc9ebbefe029d5ec33d809cf
尾花 <47906698+leonardoNael@users.noreply.github.com>
2024-11-29 23:20:08 -0300

    footer adicionado

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,16 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (<footer className="footer">
+        <section>
+            <img src="/imagens/logo.png" alt="" />
+        </section>
+        <section>
+            <p>
+                Desenvolvido por Alura.
+            </p>
+        </section>
+    </footer>)
+}
+
+export default Rodape
