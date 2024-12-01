dc749251dce351354178e050e417a2ca006b6b13
尾花 <47906698+leonardoNael@users.noreply.github.com>
2024-11-30 23:51:20 -0300

    input de mudar a cor do card do colaborador e inicio da função deleletar colaborador

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,8 +1,10 @@
+import { IoIosCloseCircle } from "react-icons/io";
 import './Colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (
         <div className='colaborador'>
+            <IoIosCloseCircle size={25} className='deletar' onClick={aoDeletar} />
             <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
                 <img src={colaborador.imagem} alt={colaborador.nome} />
             </div>
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,16 +1,31 @@
 import Colaborador from '../Colaborador'
 import './Time.css'
 
-const Time = ({ time, colaboradores }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
+        colaboradores.length > 0 && <section 
+            className='time' 
+            style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
 
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corSecundaria }}>
-            <h3 style={{ borderColor: time.corPrimaria }}>{time.nome}</h3>
-            <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corPrimaria} />)}
-            </div>
+                <input 
+                    onChange={evento => mudarCor(evento.target.value, time.nome)} 
+                    value={time.corPrimaria} 
+                    type='color' 
+                    className='input-cor' />
+
+                <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
+
+                <div className='colaboradores'>
+                    {colaboradores.map((colaborador, indice) => {
+                        return <Colaborador 
+                        key={indice} 
+                        colaborador={colaborador} 
+                        corDeFundo={time.cor}
+                        aoDeletar={aoDeletar} />
+                    })}
+                </div>
+
         </section>
-
     )
 }
 
