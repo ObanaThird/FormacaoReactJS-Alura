8e14146790eab16f9ea9c10e78585315d7d3e37b
尾花 <47906698+leonardoNael@users.noreply.github.com>
2024-12-03 19:33:32 -0300

    refatorando o código para o campo de cor do formulário de times

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Botao from '../Botao'
-import CampoTexto from '../CampoTexto'
+import Campo from '../Campo'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
@@ -31,21 +31,21 @@ const Formulario = ({aoCadastrar, times, cadastrarTime}) => {
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto 
+                <Campo 
                     obrigatorio
                     label="Nome" 
                     placeholder="Digite seu nome" 
                     valor={nome} 
                     aoAlterado={valor => setNome(valor)} />
 
-                <CampoTexto 
+                <Campo 
                     obrigatorio
                     label="Cargo" 
                     placeholder="Digite seu cargo"
                     valor={cargo} 
                     aoAlterado={valor => setCargo(valor)} />
 
-                <CampoTexto 
+                <Campo 
                     label="Imagem" 
                     placeholder="Digite o endereço da imagem" 
                     valor={imagem} 
@@ -67,15 +67,16 @@ const Formulario = ({aoCadastrar, times, cadastrarTime}) => {
                 setCorTime('')
             }}>
                 <h2>Preencha os dados para criar um novo time</h2>
-                <CampoTexto 
+                <Campo 
                     obrigatorio
                     label="Nome do time" 
                     placeholder="Digite o nome do time" 
                     valor={nomeTime} 
                     aoAlterado={valor => setNomeTime(valor)} />
 
-                <CampoTexto 
+                <Campo 
                     obrigatorio
+                    type='color'
                     label="Cor" 
                     placeholder="Digite a cor do time"
                     valor={corTime} 
